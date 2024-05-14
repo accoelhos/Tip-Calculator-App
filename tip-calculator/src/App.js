4138d34c369b9209a0eaaedf6ca90ba5ce402867
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-05-14 18:41:16 -0300

    create project

---
+++ tip-calculator/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+import "./index.css";
+
+export default function App(){
+
+  return(
+    <>
+      <div>
+        
+      </div>
+    
+    
+    
+    </>
+  )
+
+}
